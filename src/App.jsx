7230d9029eb3286ca3006ae9b64b15fb8e01804f
Abner-PB <152179406+APB-Abner@users.noreply.github.com>
2024-10-30 00:11:00 -0300

    Refactor: Remove unused useState import in App component and update Vite configuration to include support for .hdr files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import Modelo from './components/Modelo'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
